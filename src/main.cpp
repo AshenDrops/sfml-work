924a0dd684c7f12fa1063f1140f886436ebe2719
Ashlynn <ashlynn.wonderland@gmail.com>
2015-08-04 23:31:41 -0400

    Window is staying open and reacts to close events now

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-  sf::Window window(sf::VideoMode(600, 800), "Test Window");
+  // Create window -- 200x200 for each "tile"
+  sf::Window window(sf::VideoMode(600, 600), "Tic-tac-toe", sf::Style::Close);
+
+  //VSync
+  window.setVerticalSyncEnabled(true);
+
+  // Main loop
+  while(window.isOpen()){
+
+    // Go through all of the events
+    sf::Event event;
+    while(window.pollEvent(event)){
+
+      // If close event, close
+      if(event.type == sf::Event::Closed){
+        window.close();
+      }
+
+
+    }
+  }
 
   return 0;
 }
